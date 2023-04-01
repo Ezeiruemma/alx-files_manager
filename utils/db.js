cf1e1b40197018618e131f54de279c21e2ae1d48
Onyedikachi Onu <okc4pardon@gmail.com>
2023-04-01 13:04:53 -0700

    :beetle: completed db config

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,20 @@ import mongodb from 'mongodb';
 
 class DBClient{
     constructor(){
-	this.client = new mongodb.MongoClient(`mongodb://${process.env.DB_HOST || '127.0.0.1'}:${process.env.DB_PORT ||  27017}/${process.env.DB_DATABASE || 'files_manager'}`);
+	this.db = {connected: false};
+	this.client = new mongodb.MongoClient(`mongodb://${process.env.DB_HOST || '127.0.0.1'}:${process.env.DB_PORT ||  27017}/${process.env.DB_DATABASE || 'files_manager'}`, { useUnifiedTopology: true });
+	this.client.on('open', _=>this.db.connected = true);
 	this.client.connect();
+    }
+    isAlive(){
+	// return this.client.isConnected();
+	return this.db.connected;
+    }
+    nbUsers(){
+	this.client.db().collection('users').countDocuments();
+    }
+    nbFiles(){
+	this.client.db().collection('files').countDocuments();
     }
 }
 
